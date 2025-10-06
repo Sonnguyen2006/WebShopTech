d5a081b7dccce7758419278d65fdb9086a5b8e80
son <sonnguyenminh13231@gmail.com>
2025-10-06 11:33:45 +0700

    add view of show product

---
+++ resources/views/products/show.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container my-5">
+  <div class="row">
+    <div class="col-md-5">
+      <img src="{{ asset('public/images/' . $product->product_image) }}" 
+           class="img-fluid rounded" 
+           alt="{{ $product->product_name }}">
+    </div>
+    <div class="col-md-7">
+      <h3>{{ $product->product_name }}</h3>
+      <p class="text-muted">Mã sản phẩm: {{ $product->product_id }}</p>
+      <h4 class="text-danger fw-bold mb-3">
+        {{ number_format($product->product_price, 0, ',', '.') }}đ
+      </h4>
+      <p>{{ $product->product_description }}</p>
+
+      <a href="{{ url()->previous() }}" class="btn btn-outline-secondary mt-3">⬅ Quay lại</a>
+    </div>
+  </div>
+</div>
+@endsection
