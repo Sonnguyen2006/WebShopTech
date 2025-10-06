57fce60e0c4bb336ce35333105987d55b4ec1f32
son <sonnguyenminh13231@gmail.com>
2025-10-06 11:31:41 +0700

    add show product in productcontroller

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,9 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\ProductModel;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    
+    public function show($product_id ){
+        $product = ProductModel::where('product_id', $product_id )->FirstOrFail();
+        return view ('product.show',compact('product'));
+    }
 }
