9736d1b738027616a3e6c33ec274e440b11fb5c8
sonnguyen <nguyenminhson13231@gmail.com>
2025-11-22 00:52:56 +0700

    add primary key for orderDetailModel

--- app/Models/OrderDetail.php
+++ app/Models/OrderDetail.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class OrderDetail extends Model
 {
     protected $table = 'order_details';
+    protected $primaryKey = 'order_details_id';
     protected $fillable = ['order_id', 'product_id', 'quantity', 'product_cost'];
 
     public function order()
